573d5c5d738c8c348ca3efb4814dab98d74f47ee
Bright Light <125852184+prostar15@users.noreply.github.com>
2024-02-22 00:06:12 +0900

    Delete Assets directory

--- Assets/ReadOnlyAttribute/Scripts/ReadOnlyAttribute.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Use this PropertyAttribute to make a public variable uneditable in the Inspector
-/// </summary>
-public class ReadOnlyAttribute : PropertyAttribute { }
